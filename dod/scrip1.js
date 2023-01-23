9aef065c16f4d257c2292bb3954667f310d13b6c
Michcio1208 <michalkrystecki1208@gmail.com>
2023-01-23 19:37:04 +0100

    1

---
+++ dod/scrip1.js
@@ -0,0 +1,14 @@
+var tab=new Array()//zamiana
+for(let i=0;i<10;i++)
+  {
+    tab.push(parseInt(prompt("Podaj element: ")))
+  }
+console.log(tab)
+for(let i =0;i<5;i++)//bo dwa na raz sie zamienia
+{ 
+  let temp=tab[i]
+  [tab[i],tab[10-i-1]]=[tab[10-i-1],tab[i]]
+ //alternatywnie/lepiej [tab[i],tab[10-i-1]]=[tab[10-i-1],tab[i]]
+ //alternatywnie tab[i]=tab[10-i-1] tab[10-i-1]=temp;
+ 
+      }console.log(tab)

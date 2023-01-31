f1e0ab5f03ad2b1d3f42c5282d17bf0393098b3b
Michcio1208 <93605739+Michcio1208@users.noreply.github.com>
2023-01-31 11:44:04 +0100

    Add files via upload

---
+++ 2klasa/Javascript/31.01.2023/31-01-2023/README.md
@@ -0,0 +1,4 @@
+# 31.01.2023
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Michcio1208/pen/ZEjqELQ](https://codepen.io/Michcio1208/pen/ZEjqELQ).
+
---
+++ 2klasa/Javascript/31.01.2023/31-01-2023/dist/script.js
@@ -0,0 +1,36 @@
+function funkcja(){
+  let a=parseFloat(document.getElementById("pole").value)
+if(a>=0 && a<=10){
+  document.write("oke")}
+ else{
+    document.write("not oke")
+  }
+   }
+
+function haslo(){
+  let a=(document.getElementById("pole").value)
+  let b=(document.getElementById("pole1").value)
+  if(a=="login" && b=="password")
+  {
+  document.write("oke")
+  }
+  else if(a.length<=4)
+  {
+    document.write("tf2?")
+  }
+  else if(a=="" && b=="")
+  {
+    alert('tf?')
+    document.write("tf?")
+  }
+ else{
+    document.write("not oke")
+  }
+   }
+function kolor(){ //giga sus dziala do dupy
+  document.bgColor="blue"
+}
+  function kolor1(){ //giga sus dziala do dupy
+  document.bgColor="green"}
+  function kolor2(){ //giga sus dziala do dupy
+  document.bgColor="red"}

1266c56af3bca8fd95afe4005941310de5fe8717
Michcio1208 <michalkrystecki1208@gmail.com>
2024-03-29 11:38:48 +0100

    1

--- 1klasa/programyC++/1.10.21/main.cpp
+++ 1klasa/programyC++/1.10.21/main.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main()
 {
-   // int a=1;
-    //for(int i=1;i<=10;i++)
-      //  a*=i;
-     //   cout<<a<<endl;
+   int a=1;
+    for(int i=1;i<=10;i++)
+       a*=i;
+        cout<<a<<endl;
 
 
 

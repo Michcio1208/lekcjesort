f03af58c335044f9664e621433a98d1214a23a2f
Michcio1208 <michalkrystecki1208@gmail.com>
2024-04-16 10:24:57 +0200

    1

--- 3klasa/C++/palindromy.cpp
+++ 3klasa/C++/palindromy.cpp
@@ -4,12 +4,33 @@ using namespace std;
 
 int main (){
     string a;
-    int j;
+    cout<<"podaj wyraz"<<endl;
     cin>>a;
+    int i,j;
+    j=a.size() -1;
+    i=0;
+    bool palindrom= true;
+    while (palindrom && i<j)
+    {
+        if (a[i]==a[j])
+        {
+           i++;
+           j--;
+        }
+        else{
+            palindrom=false;
+        }
+    }
+    if (palindrom)
+    {
+        cout<<"TAK";
+    }
+    else{
+        cout<<"NIe";
+    }
+    
 
-for(int i=0; i<j;i++){
 
-}
 
 
 

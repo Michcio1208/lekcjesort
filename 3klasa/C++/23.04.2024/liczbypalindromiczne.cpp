5cbd0b37aabf1bd4881eb7ffdf2f0ab08452cb74
Michcio1208 <michalkrystecki1208@gmail.com>
2024-04-23 10:06:25 +0200

    1

--- 3klasa/C++/23.04.2024/liczbypalindromiczne.cpp
+++ 3klasa/C++/23.04.2024/liczbypalindromiczne.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
 #include <string>
-#include <cctype>
+#include <sstream>
 
 using namespace std;
 
-int main(){
+bool isPalindrome(int num) {
+    int reversedNum = 0;
+    int originalNum = num;
 
+    while (num > 0) {
+        int digit = num % 10;
+        reversedNum = reversedNum * 10 + digit;
+        num /= 10;
+    }
 
+    return originalNum == reversedNum;
+}
 
+int main() {
+    string input;
+    cout << "Enter a string containing numbers: ";
+    getline(cin, input);
+
+    stringstream ss(input);
+    string word;
+    int num;
+
+    cout << "Palindromic numbers found in the string:" << endl;
+
+    while (ss >> word) {
+        if (stringstream(word) >> num && isPalindrome(num)) {
+            cout << num << endl;
+        }
+    }
+
+    return 0;
 }

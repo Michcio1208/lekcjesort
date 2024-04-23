d4746eee53449abb1a10c44bcfe90a57b968511c
Michcio1208 <michalkrystecki1208@gmail.com>
2024-04-23 10:16:58 +0200

    1

---
+++ 3klasa/C++/23.04.2024/lekcyjne.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+void dp()
+{
+for(int i=1;i<=9;i++)
+{
+    for(int j=0;j<=9;j++)
+    {
+        cout<<i<<j<<i<<endl;
+}
+}
+}
+
+void other(){
+    for(int i=99;i<100;i++)
+    {
+        if(i%99-i%100==i%10)
+        {
+            cout<<i<<endl;
+        }
+    }
+}
+int main(){
+   
+}

6936fc5d27eccab8b6edbe12d62ba12ddc95c8f4
Michcio1208 <michalkrystecki1208@gmail.com>
2024-04-24 14:17:07 +0200

    1

--- 3klasa/C++/24.04.2024/sortowanie.cpp
+++ 3klasa/C++/24.04.2024/sortowanie.cpp
@@ -14,6 +14,17 @@ void bubbleSort(int arr[], int n) {
         }
     }
 }
+void bubbleSortm(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
 
 int main() {
     int n;
@@ -30,7 +41,7 @@ int main() {
     }
 
     // Sorting the table using bubble sort
-    bubbleSort(table, n);
+    bubbleSortm(table, n);
 
     cout << "Sorted table in ascending order: ";
     for (int i = 0; i < n; i++) {

3f56618e9f880024524a290ad2f7c7193e390f21
Michal Krystecki <krysteckimic>
2024-06-04 10:11:15 +0200

    lekcja

--- 3klasa/C++/04.06.2024/znajdowanie.cpp
+++ 3klasa/C++/04.06.2024/znajdowanie.cpp
@@ -8,7 +8,7 @@ int main(){
 int num;
 cout<<"podaj liczbe"<<endl;
 cin>>num;
-A[N]={1,2,3,4,5,6,7,8,9,10};
+int A[N]={1,2,3,4,5,6,7,8,9,10};
 for(int i=0; i<N;i++){
 if(A[i]==num){
     cout<<"tak na pozycji"<<i<<endl;

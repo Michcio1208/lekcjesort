53a19677d6554509d93123409bac34198a9754c4
Michcio1208 <michalkrystecki1208@gmail.com>
2024-06-07 12:22:49 +0200

    lekcja

--- 3klasa/C++/07.06.2024/podciagi.cpp
+++ 3klasa/C++/07.06.2024/podciagi.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <string>
 #include <cctype>
-#include<cmath>
+#include <cmath>
 using namespace std;
 
 
 const int N=10;
-int main(){
-    int A[N]={0,1,2,3,4,5,6,7,8,9};
-int max=1;
+
+
+int DNSPNM(int A[])
+{
+int max=1; 
 int a=1;
 for(int i=1;i<N;i++)
 {
-    if(A[i]>=A[i+1])
+    if(A[i]>=A[i-1])
     {
-     a=a+1;
+     a++;
      if(a>max)
      {
        max=a;
@@ -26,8 +28,15 @@ for(int i=1;i<N;i++)
     }
 
 }
+return max;
 
-cout << max;
+}
+int main(){
+    int A[N]={0,1,2,3,4,5,6,7,8,9};
+    cout<<DNSPNM(A)<<endl;
+
+
+
 
 
 

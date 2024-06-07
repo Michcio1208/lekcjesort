8752d7912e0d222ea832ae25e2a3cc4e735ea4a5
Michcio1208 <michalkrystecki1208@gmail.com>
2024-06-07 12:12:06 +0200

    lekcja

--- 3klasa/C++/07.06.2024/x.cpp
+++ 3klasa/C++/07.06.2024/x.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include<cmath>
 using namespace std;
 
+
+const int N=10;
 int main(){
+    int A[N]={0,1,2,3,4,5,6,7,8,9};
+int max=1;
+int a=1;
+for(int i=1;i<N;i++)
+{
+    if(A[i]>=A[i+1])
+    {
+     a=a+1;
+     if(a>max)
+     {
+       max=a;
+     }
+    }
+    else 
+    {
+        a=1;
+    }
 
+}
 
+cout << max;
 
 
 

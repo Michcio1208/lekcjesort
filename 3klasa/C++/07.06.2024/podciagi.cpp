1b84d97719044c323bd84d327e07847b81442821
Michcio1208 <michalkrystecki1208@gmail.com>
2024-06-07 12:37:40 +0200

    lekcja

--- 3klasa/C++/07.06.2024/podciagi.cpp
+++ 3klasa/C++/07.06.2024/podciagi.cpp
@@ -31,10 +31,41 @@ for(int i=1;i<N;i++)
 return max;
 
 }
+
+int NSPNM(int A[])
+{
+int max1=1;
+int b=1;
+int max0=0;
+int b0=0;
+int i;
+for (i=1;i<N;i++)
+{
+if(A[i]>=A[i-1])
+{
+    b++;
+    }
+if(b>max1)
+{
+max1=b;
+
+max0=b0;
+}
+
+else{
+    b=1;
+    b0=1;
+}
+
+}
+
+return max0;
+}
+
 int main(){
     int A[N]={0,1,2,3,4,5,6,7,8,9};
     cout<<DNSPNM(A)<<endl;
-
+   cout<<NSPNM(A)<<endl;
 
 
 

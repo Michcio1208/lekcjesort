c37cd59b4ad9d320e57eb0706334dcd56a464aff
Michcio1208 <michalkrystecki1208@gmail.com>
2024-06-12 14:20:21 +0200

    1

--- 3klasa/C++/07.06.2024/podciagi.cpp
+++ 3klasa/C++/07.06.2024/podciagi.cpp
@@ -59,22 +59,44 @@ else{
 
 }
 
-return max0;
+return max0;}
+
+int MSPS(int A[]){
+
+int maks_suma=0, akt_suma, i, j,k; 
+for ( i=0;i<N;i++){
+for(int j=i;j<N;j++)
+for(k=i;k<=j;k++)
+{
+akt_suma+=A[k];
+if(akt_suma>maks_suma){
+    maks_suma=akt_suma;
+}
 }
 
+
+
+}
+
+
+
+return maks_suma;
+}
+
+
 int main(){
-    int A[N]={0,1,2,3,4,5,6,7,8,9};
+    int A[N]={0,1,2,3,4,5,6,7,8,-9};
   /* cout<<DNSPNM(A)<<endl;
    cout<<NSPNM(A)<<endl;*/
+
+/*
  int i=NSPNM(A);
  do{
 cout<<i<<endl;
 i++;
- }while (i<N && A[i]>=A[i-1]);
+ }while (i<N && A[i]>=A[i-1]);*/
 
- 
-
-
+cout<<"Maksymalna suma: "<<MSPS(A)<<endl;
 
 
 

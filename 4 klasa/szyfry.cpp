a66f355d1d837db7a29496cdf23ae7df6ad71987
Michcio1208 <michalkrystecki1208@gmail.com>
2024-09-06 12:18:46 +0200

    1

---
+++ 4 klasa/szyfry.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+int main(){
+
+char znak; 
+for (int i=48;i<=126;i++)
+{
+    znak=i;
+    cout<<znak<<" ";
+    if(i%16==15)
+    {
+        cout<<endl;
+    }
+}
+cout<<endl; 
+
+
+
+
+
+
+
+}

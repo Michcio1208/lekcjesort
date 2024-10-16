cc93e834ece8c3b303ccf3a2372d565119d7eb7a
Michcio1208 <michalkrystecki1208@gmail.com>
2024-10-16 16:30:48 +0200

    1

--- 4 klasa/sprawdzian szyfry/sprawdzian2.cpp
+++ 4 klasa/sprawdzian szyfry/sprawdzian2.cpp
@@ -3,31 +3,22 @@
 
 using namespace std;
 
-string przestaw(string tj, string kl)
+string przestaw2(string tj, string kl)
 {
-    string szyfrogram = "";
-    int i, j;
+string szyfrogram="";
+int i,j;
+while(tj.size()%kl.size()!=0) tj=tj+'X';
+for(j=0;j<kl.size();j++)
+    for(i=0;i<tj.size()/kl.size();i++)
+        szyfrogram+=tj[i*kl.size()+kl[j]-'O'];
+        return szyfrogram;
 
-    while (tj.size() % kl.size() != 0) 
-        tj += 'X';
 
-    for (j = 0; j < kl.size(); j++)
-        for (i = 0; i < tj.size() / kl.size(); i++)
-            szyfrogram += tj[i * kl.size() + (kl[j] - '0') - 1];
-
-    return szyfrogram;
 }
 
-int main() 
-{
-    string klucz, tekst;
-    cout << "Podaj tekst do zaszyfrowania: ";
-    getline(cin, tekst);
-    cout << "Podaj klucz: ";
-    cin >> klucz;
-
-    string zaszyfrowany = przestaw(tekst, klucz);
-    cout << "Zaszyfrowany tekst: " << zaszyfrowany << endl;
-
-    return 0;
+int main() {
+  string klucz, tekst;
+getline(cin,tekst);
+cin>>klucz;
+przestaw2(tekst,klucz);
 }

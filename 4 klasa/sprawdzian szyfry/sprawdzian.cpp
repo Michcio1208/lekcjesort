5ffc60a553867a32c3d37ab51b687ad4e87b14eb
Michcio1208 <michalkrystecki1208@gmail.com>
2024-10-16 13:17:31 +0200

    1

--- 4 klasa/sprawdzian szyfry/sprawdzian.cpp
+++ 4 klasa/sprawdzian szyfry/sprawdzian.cpp
@@ -3,19 +3,19 @@
 
 using namespace std;
 
-// Funkcja szyfrująca (odwracanie tekstu wspak)
+
 string encrypt(const string &text) {
     string encrypted;
-    // Iteracja od końca do początku oryginalnego tekstu
+
     for (int i = text.length() - 1; i >= 0; i--) {
         encrypted += text[i];
     }
     return encrypted;
 }
 
-// Funkcja deszyfrująca (ponowne odwracanie tekstu wspak)
+
 string decrypt(const string &encryptedText) {
-    // Deszyfrowanie jest takie samo jak szyfrowanie, bo odwrócenie odwróconego daje oryginał
+
     return encrypt(encryptedText);
 }
 
@@ -24,11 +24,11 @@ int main() {
     cout << "Podaj tekst do zaszyfrowania: ";
     getline(cin, input);
 
-    // Szyfrowanie tekstu
+
     string encrypted = encrypt(input);
     cout << "Zaszyfrowany tekst: " << encrypted << endl;
 
-    // Deszyfrowanie tekstu
+
     string decrypted = decrypt(encrypted);
     cout << "Odszyfrowany tekst: " << decrypted << endl;
 

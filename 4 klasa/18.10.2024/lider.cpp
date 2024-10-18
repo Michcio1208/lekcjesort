4e7b544407551281fd44fbfb04757d4e9f4a9083
Michcio1208 <michalkrystecki1208@gmail.com>
2024-10-18 12:04:19 +0200

    1

---
+++ 4 klasa/18.10.2024/lider.cpp
@@ -0,0 +1,14 @@
+// liderem nazywamy taki element n-elementowego zbioru, ktory wystepuje wiecej niz n/2 razy
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+
+
+
+
+int main()
+{
+    
+}

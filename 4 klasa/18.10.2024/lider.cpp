273470edebd75349a05aab5bd7a1161782fe9f95
Michcio1208 <michalkrystecki1208@gmail.com>
2024-10-18 12:18:28 +0200

    1

--- 4 klasa/18.10.2024/lider.cpp
+++ 4 klasa/18.10.2024/lider.cpp
@@ -3,13 +3,26 @@
 #include <cctype>
 using namespace std;
 
+const int N=10;
+int szukajlidera(int A[])
+{
+for(int i=0;i<N;i++)
+{
+    int ile=0;
+    for(int j=0;j<N;j++ )
+    {
+        if(A[j]==A[i]) ile+=1;
+    }
+ if(ile>N/2) return A[i];
+}
+return -1; 
 
-
-
+}
 
 // liderem nazywamy taki element n-elementowego zbioru, ktory wystepuje wiecej niz n/2 razy
 
 int main()
 {
-    
+    int A[1,2,4,2,2,2,2,2];
+    cout<<szukajlidera(A);
 }

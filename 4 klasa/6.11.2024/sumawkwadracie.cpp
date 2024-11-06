c547ee3f59a9c9dc01521d3e2fddb25ed67759ea
Michcio1208 <michalkrystecki1208@gmail.com>
2024-11-06 13:28:22 +0100

    a

--- 4 klasa/6.11.2024/sumawkwadracie.cpp
+++ 4 klasa/6.11.2024/sumawkwadracie.cpp
@@ -25,6 +25,22 @@ void Wypisz(int A[][N]){
 
 }
 
+void makssuma(int A[][N])
+{
+int suma=A[0][0];
+int w=0;
+int k=0;
+for(int i=1;i<2*N-2;i++){
+    if(A[w+1][k]>A[w][k+1]){
+        suma=suma+A[w+1][k];
+        w=w+1;}
+    else{
+        suma=suma+A[w][k+1];
+        k=k+1;
+    }
+
+}
+}
 
 int main(){
 

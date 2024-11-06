7ebd9fccb4dd190a8a3d83f0e16688c90aebd7ad
Michcio1208 <michalkrystecki1208@gmail.com>
2024-11-06 13:19:37 +0100

    a

--- 4 klasa/6.11.2024/sumawkwadracie.cpp
+++ 4 klasa/6.11.2024/sumawkwadracie.cpp
@@ -6,11 +6,32 @@
 #include <ctime>
 using namespace std;
 
+const int N=5;
 
+void Losuj(int A[][N]){
+    for(int i=0; i<N;i++)
+        for(int j=0;j<N;j++)
+            A[i][j]=1+rand()%9;
+
+
+}
+void Wypisz(int A[][N]){
+  for(int i=0; i<N;i++)
+        for(int j=0;j<N;j++)
+            cout<<A[i][j]<<" ";
+        cout<<endl;
+
+
+
+}
 
 
 int main(){
 
+int A[N][N];
+srand(time(NULL));
+Losuj(A);
+Wypisz(A);
 
 
 
@@ -25,5 +46,4 @@ int main(){
 
 
 
-    
 }

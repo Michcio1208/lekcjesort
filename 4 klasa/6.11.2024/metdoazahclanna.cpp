cfda5912a21e8743b8373775736e77778775d7fc
Michcio1208 <michalkrystecki1208@gmail.com>
2024-11-06 13:04:18 +0100

    a

---
+++ 4 klasa/6.11.2024/metdoazahclanna.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+const int N=15;
+const int nominaly[]={50000,20000,10000,5000,2000,1000,5000,200,100,50,20,10,5,2,1};
+
+void wydajreszte(int kwota, int Reszta[])
+{
+    for(int i=0; i<N;i++)
+    {
+        Reszta[i]=kwota/nominaly[i];
+        kwota=kwota%nominaly[i];
+    }
+
+
+
+
+}
+
+int main(){
+
+
+int kwota,Reszta[N];
+cout<<"Podaj kwote w groszach"<<endl;
+cin>>kwota;
+for(int i=0;i<N;i++)
+    if(Reszta[i]>0)
+    cout<<nominaly[i]<<": "<<Reszta[i]<<endl;
+
+
+
+
+
+
+
+
+
+
+}

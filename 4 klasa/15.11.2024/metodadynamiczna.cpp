392475c573f6c4ca1671e6d9b67a91cfcaa66a80
Michcio1208 <michalkrystecki1208@gmail.com>
2024-11-15 12:23:43 +0100

    1

---
+++ 4 klasa/15.11.2024/metodadynamiczna.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+using namespace std;
+
+const int N=5;
+
+
+
+
+
+
+
+int main(){
+
+
+
+
+
+
+
+
+    ;
+}

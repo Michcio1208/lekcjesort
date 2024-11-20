469d59cbe7f5b35a72f4c57851ba3fb252cb9174
Michcio1208 <michalkrystecki1208@gmail.com>
2024-11-20 12:55:39 +0100

    1

---
+++ 4 klasa/20.11.2024/wydawaniereszty.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+using namespace std;
+
+const int N=5;
+
+
+
+int main(){
+
+
+
+
+xw
+
+
+
+
+
+
+}

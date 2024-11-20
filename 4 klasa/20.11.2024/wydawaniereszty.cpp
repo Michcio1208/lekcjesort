3bba4f3610186f6d7614e8edcb144c33adf0ae58
Michcio1208 <michalkrystecki1208@gmail.com>
2024-11-20 13:12:12 +0100

    1

--- 4 klasa/20.11.2024/wydawaniereszty.cpp
+++ 4 klasa/20.11.2024/wydawaniereszty.cpp
@@ -7,7 +7,7 @@
 #include <fstream>
 using namespace std;
 
-const int N=5;
+const int N=4;
 const int nominaly[]={1,2,4,5};
 const int kwota=8;
 
@@ -15,8 +15,8 @@ int ileMonet(){
 int i,j;
 int IleM[kwota+1];
 IleM[0]=0;
-for(int i=0;i<=kwota;i++) IleM[i]=kwota+1;
-for(int i=0;i<=kwota;i++)  
+for(int i=1;i<=kwota;i++) IleM[i]=kwota+1;
+for(int i=1;i<=kwota;i++)  
     for(int j=0;j<N;j++)
         if(nominaly[j]<=i)
             if(IleM[i-nominaly[j]]+1<IleM[i])
@@ -26,7 +26,7 @@ return IleM[kwota];
 }
 
 int main(){
-cout<<ileMonet;
+cout<<"minimlna liczba monet to "<<ileMonet()<<endl;;
 
 
 

e66a78e0a6b64bf7e778b68b23d1078047921351
Michcio1208 <michalkrystecki1208@gmail.com>
2024-11-20 13:06:37 +0100

    1

--- 4 klasa/20.11.2024/wydawaniereszty.cpp
+++ 4 klasa/20.11.2024/wydawaniereszty.cpp
@@ -8,15 +8,29 @@
 using namespace std;
 
 const int N=5;
+const int nominaly[]={1,2,4,5};
+const int kwota=8;
 
+int ileMonet(){
+int i,j;
+int IleM[kwota+1];
+IleM[0]=0;
+for(int i=0;i<=kwota;i++) IleM[i]=kwota+1;
+for(int i=0;i<=kwota;i++)  
+    for(int j=0;j<N;j++)
+        if(nominaly[j]<=i)
+            if(IleM[i-nominaly[j]]+1<IleM[i])
+                IleM[i]=IleM[i-nominaly[j]]+1;
+return IleM[kwota];
 
+}
 
 int main(){
 
 
 
 
-xw
+
 
 
 

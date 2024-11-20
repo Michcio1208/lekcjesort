53b95d99d52e0a9aa4ba63e6023f7ffa1dc40c27
Michcio1208 <michalkrystecki1208@gmail.com>
2024-11-20 13:27:17 +0100

    1

--- 4 klasa/20.11.2024/wydawaniereszty2.cpp
+++ 4 klasa/20.11.2024/wydawaniereszty2.cpp
@@ -11,29 +11,35 @@ const int N=4;
 const int nominaly[]={1,2,4,5};
 const int kwota=8;
 
-int ileMonet(){
-int i,j;
+
+int resztadynamicznie2tablice(int Reszta[]){
+int i,j,k;
 int IleM[kwota+1];
+int ResztaM[kwota+1][j];
 IleM[0]=0;
+int ResztaM[0][i];
 for(int i=1;i<=kwota;i++) IleM[i]=kwota+1;
+for(int i=0;i<N;i++) 
 for(int i=1;i<=kwota;i++)  
     for(int j=0;j<N;j++)
         if(nominaly[j]<=i)
             if(IleM[i-nominaly[j]]+1<IleM[i])
+            {
                 IleM[i]=IleM[i-nominaly[j]]+1;
+                for(int k=0;k<N;k++)
+                    ResztaM[i][k]=ResztaM[i-nominaly[j]][k];
+                        ResztaM[i][j]++;
+            }
+for(i=0;i<N;i++) Reszta[i]=ResztaM[kwota][i];
 return IleM[kwota];
 
 }
 
-int main(){
-cout<<"minimlna liczba monet to "<<ileMonet()<<endl;
 
 
-
-
-
-
-
+int main (){
+int Reszta[N];
+cout<<"minimlna liczba monet to dla kwoty "<<kwota<<" "<<resztadynamicznie2tablice(Reszta)<<endl;
 
 
 

67ef8d16088b66a6da7fb8fa50dbaac0e2122113
Michcio1208 <michalkrystecki1208@gmail.com>
2024-11-22 12:39:10 +0100

    1

---
+++ 4 klasa/22.11.2024/plecak4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+using namespace std;
+
+const int makswaga=17;
+const int N=5;
+
+struct Przedmiot{
+    int waga, wart; 
+};
+
+
+int plecakdynamicznie2(Przedmiot){
+
+//na obrazku
+
+
+
+}
+
+
+int main(){
+
+
+
+
+}

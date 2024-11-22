153cf33b4d62183b3b1de9a2341d6e3bd1e56cc3
Michcio1208 <michalkrystecki1208@gmail.com>
2024-11-22 12:28:36 +0100

    1

---
+++ 4 klasa/22.11.2024/plecak3.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+using namespace std;
+
+const int makswaga = 17; // Maksymalna waga plecaka
+const int N = 5;         // Liczba przedmiotów
+
+struct Przedmiot {
+    int wart, waga;
+};
+
+int plecakdynamicznie(Przedmiot P[]) {
+    int W[makswaga + 1] = {0}; // Tablica wartości dynamicznych, inicjalizowana zerami
+
+    for (int i = 1; i <= makswaga; i++) {
+        for (int j = 0; j < N; j++) {
+            if (P[j].waga <= i) { // Jeśli waga przedmiotu mieści się w bieżącej wadze
+                W[i] = max(W[i], W[i - P[j].waga] + P[j].wart);
+            }
+        }
+    }
+    return W[makswaga];
+}
+
+int main() {
+    Przedmiot P[N];
+    ifstream wej("do-plecaka.txt");
+    if (!wej) { // Sprawdź, czy plik został poprawnie otwarty
+        cerr << "Blad: nie mozna otworzyc pliku 'do-plecaka.txt'." << endl;
+        return 1;
+    }
+
+    for (int i = 0; i < N; i++) {
+        wej >> P[i].wart >> P[i].waga;
+        if (wej.fail()) { // Sprawdź, czy wczytywanie zakończyło się sukcesem
+            cerr << "Blad: niepoprawne dane w pliku." << endl;
+            return 1;
+        }
+    }
+    wej.close();
+
+    cout << "Maksymalna wartosc plecaka to: " << plecakdynamicznie(P) << endl;
+

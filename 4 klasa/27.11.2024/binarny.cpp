e75e6581c044e8f45fa3cb643e286928c2b1721b
Michcio1208 <michalkrystecki1208@gmail.com>
2024-11-27 13:00:46 +0100

    1

---
+++ 4 klasa/27.11.2024/binarny.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+using namespace std;
+
+
+
+
+
+
+
+int main(){
+
+// 1101,11 = 1*2^3 + 1*2^2 + 0*2^1 +1*2^0 +1*2^-1 + 1*2^-2= 8+4+0+1+1/2+1/4= 13+3/4
+
+// 101111,01101 = 1*2^5 + 0*2^4 + 1*2^3 + 1*2^2 + 1*2^1 + 1*2^0 + 0*2^-1 + 1*2^-2 + 1*2^-3 + 0*2^-4 + 1*2^-5 = 32+8+4+2+1+1/4+1/8+1/32= 47+ 13/32
+
+}

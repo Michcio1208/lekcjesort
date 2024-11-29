e2cfc051a16a3cff549ff66d4898b9649d5c9097
Michcio1208 <michalkrystecki1208@gmail.com>
2024-11-29 12:15:11 +0100

    1

---
+++ 4 klasa/29.11.2024/binarny2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+using namespace std;
+
+
+
+
+
+
+
+int main(){
+
+/*
+odejmowanie binarnych
+najlepiej pisemnie
+
+1-1=0
+
+10 - 1 = 1 
+
+ 110110 
+-  1010
+ 101100    
+
+mnozenie
+
+ 110111
+*  1011
+
+  tak jak normalne mnozenie 
+
+
+
+
+
+
+*/
+}

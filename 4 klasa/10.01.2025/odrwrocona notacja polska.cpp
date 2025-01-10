bdef7f4c5706282b2a9350945e03140ce5221bda
Michcio1208 <michalkrystecki1208@gmail.com>
2025-01-10 12:33:19 +0100

    1

--- 4 klasa/10.01.2025/odrwrocona notacja polska.cpp
+++ 4 klasa/10.01.2025/odrwrocona notacja polska.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
+#include <string>
+#include <stack>
 using namespace std;
 
 
-int main(){
+
 
 /*
 
@@ -53,11 +54,62 @@ zamieniamy z notacji tradycyjnej na notacje polską (za pomocą stosu):
 pojebane gowno jakas tabelka wpisujesz na stos i potem spisujesz 
 
 
-
+BIBLIOTEKA STACK
 
 */
+string ONP(string w){
+    stack<char> stos;
+    stos.push('#');
+    string onp="";
+    for(int i=0;i<w.size();i++){
+        if(w[i]=='('){
+            stos.push('(');
+            continue;}
+        if(w[i]==')'){
+            while(stos.top()!='('){
+            onp=onp+stos.top();
+            stos.pop();}
+
+            stos.pop();
+            continue;
+
+        }
+        if(w[i]=='+' || w[i]=='-'){
+            while (stos.top()!='#' && stos.top()!='(')
+            {
+                onp=onp+stos.top();
+                stos.pop();
+            }
+            
+        }
+       if(w[i]=='*' || w[i]=='/'){
+            while (stos.top()!='*' && stos.top()!='/')
+            {
+                onp=onp+stos.top();
+                stos.pop();
+            }
+            stos.push(w[i]);
+        }
+        else onp=onp+w[i];
+
+    }
+  while(stos.top()!='#'){
+    onp=onp+stos.top();
+    stos.pop();
+  }
+stos.pop();
+return onp;
+
+}
 
 
+
+
+int main(){
+string w;
+cout<<"podaj wyrazenie algebraiczne: "<<endl;
+cin>>w;
+cout<<"Odwrotna notacja polska: "<<ONP(w)<<endl;
 
 
 

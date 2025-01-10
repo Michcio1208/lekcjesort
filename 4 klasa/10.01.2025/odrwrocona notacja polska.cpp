796bef5daee402441d3af471c15e608b24643fa2
Michcio1208 <michalkrystecki1208@gmail.com>
2025-01-10 12:20:52 +0100

    1

---
+++ 4 klasa/10.01.2025/odrwrocona notacja polska.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+int main(){
+
+/*
+
+2+3=5  - notacja tradycyjna
+
+
++ab   - notacja polska
+
++32 = 5 
+
+ab+   - odwrocona notacja polska 
+
+42+ = 6
+
+
+a*b+c -> abc+*  a*to cale w srodku czyli bc+(b+c)
+
+a+b*c ->abc*+
+a*b+c->ab*c+
+(a-b)c->ab-c*
+a/(b+c)->abc+/
+
+
+
+komendy na stos:
+
+push() dodaje do stosu, zawsze na gore
+pop()  zdejmuje z gory
+top() wyswietla ten na samej gorze
+empty() sprawdza czy stos jest pusty
+
+
+stos:
+-   LFO- last in , first out
+-
+-
+-
+-
+-
+- 
+
+
+
+zamieniamy z notacji tradycyjnej na notacje polską (za pomocą stosu):
+pojebane gowno jakas tabelka wpisujesz na stos i potem spisujesz 
+
+
+
+
+*/
+
+
+
+
+
+
+}

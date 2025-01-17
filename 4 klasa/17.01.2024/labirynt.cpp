43499d25c8dfa2c9565c57f40edc055bcc795226
Michcio1208 <michalkrystecki1208@gmail.com>
2025-01-17 12:11:34 +0100

    1

---
+++ 4 klasa/17.01.2024/labirynt.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <fstream>
+#include <stack>
+#include <iomanip>
+using namespace std;
+const int N=20;
+
+void wczytajlabirynt(int lab[][N]){
+    string s;
+    ifstream we("labirynt.txt");
+    for(int i=0;i<N;i++){
+        we>>s;
+        for(int j=0;j<N;j++){
+            if(s[j]=='#') lab[i][j]=-1;
+            else lab[i][j]=0;
+        }
+    }
+we.close();
+
+}
+
+void wypiszlabirynt(int lab[][N]){
+    cout<<"   ";
+        for(int j=0;j<N;j++){
+            cout<<setw(3)<<j;
+        }
+        cout<<endl;
+        for(int i=0;i<N;i++){
+            cout<<setw(3)<<i;
+            for(int j=0;j<N;j++){
+                if(lab[i][j]==-1) cout<<"  X";
+                else cout<<"   ";
+            }
+            cout<<endl;
+        }
+    }
+
+int main(){
+
+
+}

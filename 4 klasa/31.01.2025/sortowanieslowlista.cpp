60fe77fd1475dea6383599495b02976c9f38480c
Michcio1208 <michalkrystecki1208@gmail.com>
2025-01-31 11:45:34 +0100

    1

--- 4 klasa/31.01.2025/sortowanieslowlista.cpp
+++ 4 klasa/31.01.2025/sortowanieslowlista.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <list>
-
+#include <string>
+#include <fstream>
 using namespace std;
+const string alfabet="aąbcćdeęfghijklłmnńoóprsśtuwyzźż";
+const int N=35;
+void Buduj liste
 
-
-
+//z pliku
 //wir,kra,kwiat,wiatr,świat,świt,kraj
 //jakies sortowanie kubełkowe 
 

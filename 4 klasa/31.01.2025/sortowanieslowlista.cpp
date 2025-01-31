1eede7ec2850ef1d4ee4b87d4173bdd94aa3fc78
Michcio1208 <michalkrystecki1208@gmail.com>
2025-01-31 11:38:24 +0100

    1

---
+++ 4 klasa/31.01.2025/sortowanieslowlista.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+
+
+//wir,kra,kwiat,wiatr,świat,świt,kraj
+//jakies sortowanie kubełkowe 
+
+
+int main() {
+int n,k;
+list<int> lista;
+list<int>::iterator it;
+cout<<"n= "; cin>>n;
+cout<<"k= "; cin>>k;
+
+
+
+
+}

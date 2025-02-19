aba2d320c79d20d8c92599fb5e30f6c5e6f96f10
Michcio1208 <michalkrystecki1208@gmail.com>
2025-02-19 13:32:59 +0100

    1

---
+++ 4 klasa/19.02.2025/grafy.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <list>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+/*
+Graf – struktura matematyczna służąca do przedstawiania i badania relacji między obiektami . 
+ W uproszczeniu graf to zbiór wierzchołków, 
+które mogą być połączone krawędziami w taki sposób, 
+że każda krawędź kończy się i zaczyna w którymś z wierzchołków.
+*/
+
+
+int main(){;}

fc958d918ea3370113479e39ec4ad1837c70e587
Michcio1208 <michalkrystecki1208@gmail.com>
2025-02-19 13:12:09 +0100

    1

--- 4 klasa/31.01.2025/sortowaniekubelkamizpliku.cpp
+++ 4 klasa/31.01.2025/sortowaniekubelkamizpliku.cpp
@@ -2,70 +2,74 @@
 #include <list>
 #include <string>
 #include <fstream>
+#include <queue>
+
 using namespace std;
-const string alfabet="aąbcćdeęfghijklłmnńoóprsśtuwyzźż";
-const int N=35;
-void Budujliste(list<string> &lista, int&mdl){
+
+const string alfabet = "aąbcćdeęfghijklłmnńoóprsśtuwyzźż";
+const int N = 35;
+
+void Budujliste(list<string> &lista, int &mdl) {
     ifstream we("slowa.txt");
     string s;
-    while(we>>s){
-        if(s.size()>mdl){
-            mdl=s.size();
+    while (we >> s) {
+        lista.push_back(s);
+        if (s.size() > mdl) {
+            mdl = s.size();
         }
     }
     we.close();
 }
 
-//z pliku
-//wir,kra,kwiat,wiatr,świat,świt,kraj
-//jakies sortowanie kubełkowe 
-
-void Zapiszliste(list<string> &lista){
+void Zapiszliste(const list<string> &lista) {
     ofstream wy("slowa.txt");
-    list<string>::iterator it;
-    for( it=lista.begin(); it!=lista.end(); it++){
-        wy<<*it<<endl;
+    for (const auto &s : lista) {
+        wy << s << endl;
     }
     wy.close();
+}
 
-
-    void Sortujslowa(lista<string> &lista, int mdl){
-        queue<string> kubelki[N];
-        list<string> kubelek[N];
-        list<string>::iterator it;
-        int i,j;
-        string s;
-        for(i=mdl-1;i>=0;i--)
-        it=lista.begin();
-        while(it!=lista.end()){
-            s=*it;
-            if(s.size()>i){
-                it=lista.erase(it);
-                j=alfabet.find(s[i]);
-                kubelek[j].push_back(s);
-                
-            }
-            else{
-                it++;
+void Sortujslowa(list<string> &lista, int mdl) {
+    queue<string> kubelki[N];
+    list<string> kubelek[N];
+    list<string>::iterator it;
+    int i, j;
+    string s;
+    
+    for (i = mdl - 1; i >= 0; i--) {
+        it = lista.begin();
+        while (it != lista.end()) {
+            s = *it;
+            if (s.size() > i) {
+                it = lista.erase(it);
+                j = alfabet.find(s[i]);
+                if (j != string::npos) {
+                    kubelek[j].push_back(s);
+                }
+            } else {
+                ++it;
             }
         }
         
-    } for(j=0;j<N;j++){
-        for(it=kubelek[j].begin();it!=kubelek[j].end();it++){
-            lista.push_back(kubelki[j]).front();
-            kubelki[j].pop();
+        for (j = 0; j < N; j++) {
+            for (it = kubelek[j].begin(); it != kubelek[j].end(); it++) {
+                lista.push_back(*it);
+            }
+            kubelek[j].clear();
         }
     }
 }
 
 int main() {
-int n,k;
-list<int> lista;
-list<int>::iterator it;
-cout<<"n= "; cin>>n;
-cout<<"k= "; cin>>k;
-
-
-
-
+    int n, k, mdl = 0;
+    list<string> lista;
+    
+    cout << "n= "; cin >> n;
+    cout << "k= "; cin >> k;
+    
+    Budujliste(lista, mdl);
+    Sortujslowa(lista, mdl);
+    Zapiszliste(lista);
+    
+    return 0;
 }

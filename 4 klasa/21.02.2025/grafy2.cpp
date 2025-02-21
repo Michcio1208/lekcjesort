7fa74586e9fe25a87f2d3b8cc26fb86667da4350
Michcio1208 <michalkrystecki1208@gmail.com>
2025-02-21 11:12:22 +0100

    1

--- 4 klasa/21.02.2025/grafy2.cpp
+++ 4 klasa/21.02.2025/grafy2.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 int main(){
+    /*
 vector<int> tab;
 int n;
 cin>>n;
@@ -17,5 +18,32 @@ for(int i=0;i<tab.size();i++){
 
 for(int i=0;i<tab.size();i++){
     cout<<tab[i]<<" ";
+}*/
+
+vector<vector<int> > tab;
+int n;
+cin>>n;
+tab.resize(n);
+for(int i=0;i<tab.size();i++){
+    tab[i].resize(i+1);
+    for(int j=0;j<tab[i].size();j++){
+        tab[i][j]=rand()%100;
+    }
 }
+
+for(int i=0;i<tab.size();i++){
+    for(int j=0;j<tab[i].size();j++){
+        cout<<tab[i][j]<<" ";
+    }
+}
+
+
+
+
+
+
+
+
+
+
 }

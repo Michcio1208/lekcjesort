531ee7fc06c78195bd09829abdbdabf41638bb90
Michcio1208 <michalkrystecki1208@gmail.com>
2025-03-05 13:28:04 +0100

    1

--- 4 klasa/05.03.2025/wielomian.cpp
+++ 4 klasa/05.03.2025/wielomian.cpp
@@ -34,25 +34,26 @@ int Horner(vector<float> &A, float x){
     return y;
 }
 
-int oblicz(vector<float> &A){
-    int n = A.size()-1;
-    float y=A[n];
-    for(int i = n-1; i >= 0; i--){
-        y = y*2 + A[i];
-    }
-    return y;
-}
+int oblicz(int A[], int n){
+   long w=A[0];
+   for(int i=1; i<=n; i++){
+       w=w*2+A[i];
+    return w;
+}}
 
 int main(){
-
+int B[7]={1,0,1,1,0,1,1};
 vector<float> A;
 CzytajWielomian(A);
 int x;
 cout<<"x: ";
 cin>>x;
+int n;
+cout<<"podaj stopien 2: ";
+cin>>n;
 cout<<"W("<<x<<") = "<<W(A,x)<<endl;
 cout<<"W("<<x<<") = "<<Horner(A,x)<<endl;
-
+cout<<"Liczba dziesitena to: "<<oblicz(B,n)<<endl;
 
 
 

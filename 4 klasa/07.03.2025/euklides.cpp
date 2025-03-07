031fd7ec748079682facc50cb5e2790dee58ea38
Michcio1208 <michalkrystecki1208@gmail.com>
2025-03-07 11:17:21 +0100

    1

---
+++ 4 klasa/07.03.2025/euklides.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <stack>
+using namespace std;
+
+int NWD(int a, int b){
+    if(b==0) return a;{
+    return(NWD(b,a%b));
+    }
+}
+
+int main(){
+int a,b;
+cout<<"a: ";
+cin>>a;
+cout<<"b: ";
+cin>>b;
+cout<<"NWD("<<a<<","<<b<<") = "<<NWD(a,b)<<endl;
+
+
+
+
+}
